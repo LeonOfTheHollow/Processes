bd871fdddbc31b3e6362b518365a77f9ede84ce4
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-23 17:27:41 -0500

    ex5 completed

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,31 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
+    int p[2];
+    char buf[16];
+
+    if (pipe(p) < 0) {
+        printf("Pipe failed.\n");
+        exit(1);
+    }
+
+    int rc = fork();
+
+    if (rc < 0) {
+        printf("Fork failed.\n");
+        exit(1);
+    } else if (rc == 0) {
+        write(p[1], msg1, 16);
+        write(p[1], msg2, 16);
+        write(p[1], msg3, 16);
+    } else {
+        // int wc = waitpid(rc, NULL, 0);
+
+        for (int i = 0; i < 3; i++) {
+            read(p[0], buf, 16);
+            printf("%s\n", buf);
+        }
+    }
     
     return 0;
 }
